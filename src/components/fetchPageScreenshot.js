6cdea75245ffdae57c4f19406f383a6b046be5fe
meerakuanar <68986009+meerakuanar@users.noreply.github.com>
2023-09-18 11:25:14 +0530

    second commit

--- src/components/fetchPageScreenshot.js
+++ src/components/fetchPageScreenshot.js
@@ -1,35 +1,33 @@
-import  { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const useFetchPageScreenshot = ({ url }) => {
-  const [img, setImg] = useState('');
- 
+  const [img, setImg] = useState("");
 
   const fetchImg = async () => {
-     // Set loading to true while the API call is in progress
+    // Set loading to true while the API call is in progress
     try {
-      const username = 'mohapatra.manish.99@gmail.com';
-      const password = 'd8ecf5d93a2b3bcf';
-     
+      const username = "mohapatra.manish.99@gmail.com";
+      const password = "d8ecf5d93a2b3bcf";
 
       const statusResponse = await axios.post(
-      //  'https://api.dataforseo.com/v3/on_page/page_screenshot',
-        [{ url , "full_page_screenshot":false }],
+        "https://api.dataforseo.com/v3/on_page/page_screenshot",
+        [{ url, full_page_screenshot: false }],
         {
           auth: {
             username,
             password,
           },
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
-      
-     setImg(statusResponse?.data?.tasks[0]?.result[0]?.items[0]?.image)
+
+      setImg(statusResponse?.data?.tasks[0]?.result[0]?.items[0]?.image);
     } catch (error) {
-      console.error('API call failed:', error);
-    } 
+      console.error("API call failed:", error);
+    }
   };
 
   useEffect(() => {
